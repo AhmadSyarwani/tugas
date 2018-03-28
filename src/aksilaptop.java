f9aeda373c787beb2088ba41298b095a88c17930
AhmadSyarwani <32427962+AhmadSyarwani@users.noreply.github.com>
2018-03-28 16:36:23 +0800

    nilai atribut

--- src/aksilaptop.java
+++ src/aksilaptop.java
@@ -11,5 +11,11 @@
 public class aksilaptop {
     public static void main(String[] args){
         laptop adul = new laptop ();
+        
+        adul.merek="acer";
+        adul.warna="biru";
+        adul.hdd="500GB";
+        adul.ram="DDR4";
+        adul.processor="intel cote i5";
     }
 }
